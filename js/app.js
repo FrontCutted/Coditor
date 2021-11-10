5e534e59613a8d4a0d124c54d16c19f7bfdd61a3
FrontCutted <m.m.bhrm85@gmail.com>
2021-11-10 14:25:43 +0330

    updated sw.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,3 @@
-function dl(content, filename = 'code.txt') {
-  let link = document.createElement('a');
-  link.download = filename;
-  let blob = new Blob([content], {type: 'text/plain'});
-  link.href = URL.createObjectURL(blob);
-  link.click();
-  URL.revokeObjectURL(link.href);
-}
-function cp(){
-  var codes = document.getElementById('codes').value;
-  var name = document.getElementById('filename').value;
-  dl(codes, name);
-}
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err));
-  });
-}
+import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+const el = document.createElement('pwa-update');
+document.body.appendChild(el);
