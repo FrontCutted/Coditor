77bd64e9c8f80fd294ef24d6420b62c547966f44
FrontCutted <m.m.bhrm85@gmail.com>
2021-11-10 04:54:32 +0330

    jsjjjsjshan

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
-function cp() {
-  var codes = document.getElementById('codes');
-  var filename = document.getElementById('filename').value;
-  var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(codes));
-  element.setAttribute('download', filename);
-  element.style.display = 'none';
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
+function dl(content, filename = 'code.txt') {
+  let link = document.createElement('a');
+  link.download = filename;
+  let blob = new Blob([content], {type: 'text/plain'});
+  link.href = URL.createObjectURL(blob);
+  link.click();
+  URL.revokeObjectURL(link.href);
 }
-
+function cp(){
+  var codes = document.getElementById('codes').value;
+  var name = document.getElementById('filename').value;
+  dl(codes, name);
+}
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
