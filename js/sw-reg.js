ee1bd94e125233927841882ef36552c11c20063a
FrontCutted <m.m.bhrm85@gmail.com>
2021-11-12 00:07:41 +0330

    offline

--- js/sw-reg.js
+++ js/sw-reg.js
@@ -1,3 +1,4 @@
-import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
-const el = document.createElement('pwa-update');
-document.body.appendChild(el);
+if ('serviceWorker' in navigator) {
+  let url = 'sw.js';
+  navigator.serviceWorker.register(url);
+}
--- js/sw.js
+++ js/sw.js
@@ -1,41 +1,57 @@
-const CACHE = "Coditor";
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-const offlineFallbackPage = "offline.html";
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+var cacheName = 'CoditorFC';
+const appShellFiles = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/app.js',
+  '/offline.html',
+  '/img/icon-m-48.png',
+  '/img/icon-m-72.png',
+  '/img/icon-m-96.png',
+  '/img/icon-m-128.png',
+  '/img/icon-m-192.png',
+  '/img/icon-m-384.png',
+  '/img/icon-m-512.png'
+];
+
+self.addEventListener('install', function(e){
+  console.log('[serviceWorker] installed!');
+  e.waitUntil((async function(){
+    const cache = await caches.open(cacheName);
+    console.log('Service Worker: Caching...');
+    await cache.addAll(appShellFiles);
+  })());
 });
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-    .then((cache) => cache.add(offlineFallbackPage))
-  );
+self.addEventListener('fetch', (e) => {
+  console.log(`Service Worker: Fetched resource ${e.request.url}`);
+  e.respondWith((async function(){
+    const r = await caches.match(e.request);
+    if(r){return r;}
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
 });
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-        if (preloadResp) {
-          return preloadResp;
-        }
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
+/*
+var cacheName = 'CoditorFC-v2';
+appShellFiles.push('/pwa-examples/js13kpwa/icons/icon-32.png');
+
+self.addEventListener('install', (e) => {
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(appShellFiles);
+  })());
 });
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => {
+      if (key === cacheName) { return; }
+      return caches.delete(key);
+    }))
+  }));
+});
+
+*/
